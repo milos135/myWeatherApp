32428596b41901e1d9ba516b9da3043711ef151e
milos <milos.micovic12@gmail.com>
2023-07-25 16:58:59 +0200

    Pritier and correction of hardcore

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 let weather = {
-"apiKey" : "4ff50176f17a1a9eec162b0203a0abad",
-fetchWeather: async function(city){
-try{
-const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
-); 
+  apiKey: "4ff50176f17a1a9eec162b0203a0abad",
+  fetchWeather: async function (city) {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+      );
 
-const data =await response.json();
-this.displayWeather(data);
-}
-catch(error){
-console.error("Error fetching weather data:",error);
-}
+      const data = await response.json();
+      this.displayWeather(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  },
+  displayWeather: function (data) {
+    const { name, weather, main, wind } = data;
+    const { description } = weather[0];
+    const { temp, humidity } = main;
+    const { speed } = wind;
 
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
 
-}, 
-displayWeather: function(data) {
-const{ name,weather,main,wind } =data;
-const { description } =weather[0];
-const { temp,humidity }= main;
-const {speed } = wind;
-
-document.querySelector(".city").innerText= "Weather in " + name;
-document.querySelector(".description").innerText = description;
-document.querySelector(".temp").innerText = temp + "°C";
-document.querySelector(".humidity").innerText =
-  "Humidity: " + humidity + "%";
-document.querySelector(".wind").innerText =
-  "Wind speed: " + speed + " km/h";
-document.querySelector(".weather").classList.remove("loading");
-document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
-
-
-
-},
-
-search: function () {
+  search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
