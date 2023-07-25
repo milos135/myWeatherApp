b265bd9827d49cca4ecc851569880f421433d413
milos <milos.micovic12@gmail.com>
2023-07-25 11:39:15 +0200

    Changes

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 let weather = {
 "apiKey" : "4ff50176f17a1a9eec162b0203a0abad",
-fetchWeather: function(sity) {
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + sity + "&units=metric&appid=" + this.apiKey).then((response) =>response.json()).then((data)=>
-this.displayWeather(data));
+fetchWeather: async function(city){
+try{
+const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+); 
+
+const data =await response.json();
+this.displayWeather(data);
+}
+catch(error){
+console.error("Error fetching weather data:",error);
+}
+
 
 }, 
 displayWeather: function(data) {
-const{ name } =data;
-const { description } =data.weather[0];
-const { temp,humidity }= data.main;
-const {speed } = data.wind;
-console.log(name,description,temp,humidity,speed);
-document.querySelector(".sity").innerText= "Weather in " + name;
+const{ name,weather,main,wind } =data;
+const { description } =weather[0];
+const { temp,humidity }= main;
+const {speed } = wind;
+
+document.querySelector(".city").innerText= "Weather in " + name;
 document.querySelector(".description").innerText = description;
 document.querySelector(".temp").innerText = temp + "°C";
 document.querySelector(".humidity").innerText =
